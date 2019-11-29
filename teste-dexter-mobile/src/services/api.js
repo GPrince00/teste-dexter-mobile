50ef2b29277a7eb175fd445c531caf03ab2bdc4e
GPrince00 <gustavoprince0@gmail.com>
2019-11-28 23:00:15 -0300

    api

---
+++ teste-dexter-mobile/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:1337/parse'
+})
+
+api.defaults.headers = {
+    'X-Parse-Application-Id': 'OSGiFZBrXxNLjN3gYDPsgi7P4a0j6fzcc2iaCKga',
+    'X-Parse-Master-Key': 'k8xm42UVuIP51wR2DswLY8NL3zgWfev8AuKUUjga'
+}
+export default api;
